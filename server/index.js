5d248e3529b3a240cfc01d238717f46598aa733e
Waheby <mohammedh98@live.com>
2024-05-06 20:33:31 +0400

    12

--- server/index.js
+++ server/index.js
@@ -10,7 +10,13 @@ import AdminModel from "./routes/admin.js";
 import CommentModel from "./routes/comments.js";
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://main--experienceexchange.netlify.app"],
+    methods: ["POST", "GET"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
