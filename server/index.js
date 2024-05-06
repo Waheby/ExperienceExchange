f03b39fbf6b172793343edec6f192dbaa3947884
Waheby <mohammedh98@live.com>
2024-05-06 22:13:26 +0400

    sd

--- server/index.js
+++ server/index.js
@@ -13,9 +13,9 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 // resolving dirname for ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-console.log(__dirname);
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// console.log(__dirname);
 
 const app = express();
 app.use(cors());
