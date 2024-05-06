6ba4402e3b3da6f455b06951c78a6ab514f04732
Waheby <mohammedh98@live.com>
2024-05-06 21:28:02 +0400

    12

--- server/index.js
+++ server/index.js
@@ -9,14 +9,24 @@ import CertsModel from "./routes/certificates.js";
 import AdminModel from "./routes/admin.js";
 import CommentModel from "./routes/comments.js";
 
+import path from "path";
+import { fileURLToPath } from "url";
+
+// resolving dirname for ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+console.log(__dirname);
+
 const app = express();
-app.use(
-  cors({
-    origin: ["https://experience-exchange-frontend.vercel.app"],
-    methods: ["POST", "GET"],
-    credentials: true,
-  })
-);
+app.use(cors());
+
+// app.use(
+//   cors({
+//     origin: ["https://experience-exchange-frontend.vercel.app"],
+//     methods: ["POST", "GET"],
+//     credentials: true,
+//   })
+// );
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -25,6 +35,14 @@ app.use("/user", UsersModel);
 app.use("/certificate", CertsModel);
 app.use("/admin", AdminModel);
 app.use("/comment", CommentModel);
+
+//use client app
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+// render client for any path
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"))
+);
 
 // app.use(bodyParser.json({ limit: "30mb", extended: true })); //for image upload
 // app.use(bodyParser.urlencoded({ limit: "30mb", extended: true })); //for image upload
