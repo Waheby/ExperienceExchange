ff23e1b0c9bc212727e03daf3ea9f9bb1784cbe0
Waheby <mohammedh98@live.com>
2024-05-06 22:32:14 +0400

    12

--- server/index.js
+++ server/index.js
@@ -30,9 +30,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use("/", (req, res) => {
-  res.send("Server is connected");
-});
+
 app.use("/post", PostModel);
 app.use("/user", UsersModel);
 app.use("/certificate", CertsModel);
