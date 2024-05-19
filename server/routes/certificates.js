55a216c1e82b70ad6097d0bbfa19bf844eb997fe
Waheby <mohammedh98@live.com>
2024-05-19 14:44:21 +0400

    12

--- server/routes/certificates.js
+++ server/routes/certificates.js
@@ -5,24 +5,26 @@ import {
   getAllCertificates,
   uploadCertificate,
   modifyCertificate,
+  getUserCertificate,
 } from "../controllers/certificates.js";
 const router = express.Router();
 
 //IMAGE FILE UPLOAD FUNCTION===========================================
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    console.log(file);
-    return cb(null, "/public/certs");
-  },
-  filename: function (req, file, cb) {
-    return cb(null, Date.now() + "_" + file.originalname);
-  },
-});
-const imageUpload = multer({ storage: storage });
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     console.log(file);
+//     return cb(null, "/public/certs");
+//   },
+//   filename: function (req, file, cb) {
+//     return cb(null, Date.now() + "_" + file.originalname);
+//   },
+// });
+// const imageUpload = multer({ storage: storage });
 //IMAGE FILE UPLOAD FUNCTION============================================
 
 router.get("/all", getAllCertificates);
-router.post("/new", imageUpload.single("file"), uploadCertificate);
+router.get("/cert", getUserCertificate);
+router.post("/new", uploadCertificate);
 router.post("/modify", modifyCertificate);
 
 export default router;
